f03db14b7a07cc2acf9fcb08dedd9c5a61e52da0
david.rhinehart <drhinehart@gmail.com>
2013-04-13 10:10:18 -0500

    adding views directory example view in events/new

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 require 'httparty'
+require 'haml'
+
+set :views, Proc.new { File.join(root, "views") }
 
 config = YAML.load_file('config.yml')
 
 get '/hi' do
   "Hello World!"
+end
+
+get '/events/new' do
+  haml :'events/new'
 end
